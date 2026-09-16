08988a095ae7c441fdbbd90661bed17ad7b3c2e7
Brahmjyot <brahmjyotsingh@gmail.com>
2026-09-16 14:22:22 +0530

    Implementing Request Validation..

---
+++ src/main/java/com/rail/app/railreservation/booking/RequestValidator.java
@@ -0,0 +1,17 @@
+package com.rail.app.railreservation.booking;
+
+import com.rail.app.railreservation.booking.dto.BookingRequest;
+import com.rail.app.railreservation.enquiry.dto.SeatEnquiryRequest;
+import com.rail.app.railreservation.login.dto.LoginRequest;
+import com.rail.app.railreservation.signup.dto.SignUpRequest;
+
+public class RequestValidator {
+
+    public static void validate(BookingRequest request){}
+
+    public static void validate(SeatEnquiryRequest request){}
+
+    public static void validate(LoginRequest request){}
+
+    public static void validate(SignUpRequest request){}
+}
--- src/main/java/com/rail/app/railreservation/booking/service/impl/BookingServiceImpl.java
+++ src/main/java/com/rail/app/railreservation/booking/service/impl/BookingServiceImpl.java
@@ -1,5 +1,6 @@
 package com.rail.app.railreservation.booking.service.impl;
 
+import com.rail.app.railreservation.booking.RequestValidator;
 import com.rail.app.railreservation.booking.dto.*;
 import com.rail.app.railreservation.booking.entity.Booking;
 import com.rail.app.railreservation.booking.entity.BookingOpen;
@@ -78,6 +79,7 @@ public class BookingServiceImpl implements BookingService {
 
         logger.info(INSIDE_BOOKING_SERVICE);
 
+        RequestValidator.validate(request);
         //Check if Train No is Valid
         Train trn = trainService.getTrainByNo(request.getTrainNo())
                 .orElseThrow(() -> new InvalidBookingException("Booking Not Allowed On Non Existent Train"));
@@ -305,8 +307,9 @@ public class BookingServiceImpl implements BookingService {
 
     public Optional<Boolean> isBookingOpen(BookingRequest request){
 
-        Optional<Boolean> isBookingOpenAsOptional = bookingOpenRepo.isBookingOpen(request.getTrainNo(),Utils.toLocalDate(request.getStartDt()),
-                Utils.toLocalDate(request.getEndDt()));
+        Optional<Boolean> isBookingOpenAsOptional = bookingOpenRepo.isBookingOpen(request.getTrainNo(),
+                                                                        Utils.toLocalDate(request.getStartDt()),
+                                                                        Utils.toLocalDate(request.getEndDt()));
 
         if(isBookingOpenAsOptional.get() == true)
             return isBookingOpenAsOptional;
